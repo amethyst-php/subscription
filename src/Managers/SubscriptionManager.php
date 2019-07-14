523f8eff093e4e737a28d8591bb7737459ea5315
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/SubscriptionManager.php
+++ src/Managers/SubscriptionManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Subscription newEntity()
+ * @method \Amethyst\Schemas\SubscriptionSchema getSchema()
+ * @method \Amethyst\Repositories\SubscriptionRepository getRepository()
+ * @method \Amethyst\Serializers\SubscriptionSerializer getSerializer()
+ * @method \Amethyst\Validators\SubscriptionValidator getValidator()
+ * @method \Amethyst\Authorizers\SubscriptionAuthorizer getAuthorizer()
+ */
 class SubscriptionManager extends Manager
 {
     use ConfigurableManager;
